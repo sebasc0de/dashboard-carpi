080f047893cedce5912edffbd017cf3ad7e74df7
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-01-30 10:45:17 -0300

    Base project

---
+++ src/config/backend.ts
@@ -0,0 +1,7 @@
+// Production or developmente variable
+const PROD_URL = process.env.PROD_URL;
+const DEV_URL = process.env.DEV_URL;
+
+const API_URL = DEV_URL;
+
+export { PROD_URL, DEV_URL, API_URL };
