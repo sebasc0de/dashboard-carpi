8f81b1a41a38490267b0cadfe5549cad320ee86f
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-02-06 10:10:40 -0300

    Base options

--- src/components/tables/Company.js
+++ src/components/tables/Company.js
@@ -2,6 +2,7 @@ import { Buttons } from './Buttons';
 import { TABLE_CONFIG } from 'config/Notifications';
 import { toast } from 'react-toastify';
 import styles from '../../styles/table.module.css';
+import { Link } from 'react-router-dom';
 
 export const Company = ({ data, labels }) => {
     const handleDelete = (param) => toast(`El product se ha borrado con exito, refresca la pagina para ver los cambios`, TABLE_CONFIG);
@@ -19,7 +20,11 @@ export const Company = ({ data, labels }) => {
                 {data.map((item) => (
                     <tr key={item.id}>
                         <Buttons handleDelete={() => handleDelete(item.name)} />
-                        <td>Nombre</td>
+
+                        <td>
+                            <Link to={`/companies/${item.id}`}>{item.name}</Link>
+                        </td>
+
                         <td>Mayorista/minorista</td>
                         <td>Direccion</td>
                         <td>Phone</td>
