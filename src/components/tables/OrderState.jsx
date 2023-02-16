b24d89a757d3518e59e61c71ffa24fbb8c105e8e
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-02-16 08:44:58 -0300

    Change order state

--- src/components/tables/OrderState.jsx
+++ src/components/tables/OrderState.jsx
@@ -8,6 +8,7 @@ import Paper from '@mui/material/Paper';
 import Popper from '@mui/material/Popper';
 import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
+import styles from '../../styles/table.module.css';
 
 const options = ['Pendiente', 'Pagado', 'Enviado'];
 
@@ -38,7 +39,7 @@ const OrderState = () => {
     };
 
     return (
-        <td>
+        <td className={styles.orderButton}>
             <ButtonGroup variant="contained" ref={anchorRef} aria-label="split-button">
                 <Button onClick={handleClick}>{options[selectedIndex]}</Button>
                 <Button
